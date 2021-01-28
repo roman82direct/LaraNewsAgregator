f27c3cd411d138e57be4f99428e674b40cbf1054
Roman <roman82direct@yandex.ru>
2021-01-28 20:01:00 +0300

    setLocale done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                     <!-- Right Side Of Navbar -->
 
                     <ul class="navbar-nav ml-auto">
-                        <p>{{ \Illuminate\Support\Facades\Session::get('locale') }}</p>
+                        <p>{{ \Illuminate\Support\Facades\App::getLocale() }}</p>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ __('menu.lang') }}
